9a910544bfbb10c0d15524fe4be8d603b9e682ca
Michael Koelewijn <michael.koelewijn@lined.nl>
2017-05-10 11:13:41 +0200

    Reset styles and JS for boilerplate

--- Resources/public/javascript/app.bundle.js
+++ Resources/public/javascript/app.bundle.js
@@ -1,2 +1,2 @@
-!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};r.m=t,r.c=e,r.p="",r(0)}([function(t,r,e){"use strict";e(1)},function(t,r){"use strict";alert("testa")}]);
+!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};r.m=t,r.c=e,r.p="",r(0)}([function(t,r,e){"use strict";e(1)},function(t,r){"use strict"}]);
 //# sourceMappingURL=app.bundle.js.map
