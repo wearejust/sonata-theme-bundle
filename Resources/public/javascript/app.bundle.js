7326cb8f089866fab7643e5e9c287d564e5de8da
Michael Koelewijn <michael.koelewijn@lined.nl>
2017-05-10 11:11:16 +0200

    Add javascript theme file to bundle

--- Resources/public/javascript/app.bundle.js
+++ Resources/public/javascript/app.bundle.js
@@ -1,2 +1,2 @@
-!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};t.m=r,t.c=e,t.p="",t(0)}([function(r,t){"use strict"}]);
+!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};r.m=t,r.c=e,r.p="",r(0)}([function(t,r,e){"use strict";e(1)},function(t,r){"use strict";alert("testa")}]);
 //# sourceMappingURL=app.bundle.js.map
