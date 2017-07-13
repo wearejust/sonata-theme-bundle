f4dcac185609e93bb7a4b22dece830b25872074f
Bart Koelewijn <bart.koelewijn@wearejust.com>
2017-07-13 11:48:36 +0200

    Feature: Re-factor the checkboxes script with the actions bar

--- Resources/public/javascript/multipleactions.js
+++ Resources/public/javascript/multipleactions.js
@@ -1,15 +1,25 @@
+let checkboxes;
+let element;
+
 $(function(){
-    $('#list_batch_checkbox, .sonata-ba-list input').on('change', function(){
-        setTimeout(function(){ change(); }, 100);
-    });
+    element = $('.box-footer .form-inline');
+    if(!element.length) return;
+
+    checkboxes = $('#list_batch_checkbox, .sonata-ba-list input[type="checkbox"]');
+    checkboxes.on('change', change);
     change();
 });
-function change() {
-    let actionEl = $('.box-footer .form-inline');
 
-    if($('.sonata-ba-list input:checked').length) {
-        actionEl.addClass('is-visible');
-    } else {
-        actionEl.removeClass('is-visible');
-    }
+function change() {
+    setTimeout(function() {
+        toggle();
+    },100);
 }
+    
+function toggle() {
+    if(checkboxes.filter(':checked').length) {
+        element.addClass('is-visible');
+    } else {
+        element.removeClass('is-visible');
+    }
+} 
