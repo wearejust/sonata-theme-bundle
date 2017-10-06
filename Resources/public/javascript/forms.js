6e491f077467fd673207c0d1ee0608cdd78cf45e
Cees van Egmond <cees.vanegmond@wearejust.com>
2017-10-06 14:44:09 +0200

    Feature: Remove html from JS to Backend
    Query backend for righ HTML

--- Resources/public/javascript/forms.js
+++ Resources/public/javascript/forms.js
@@ -1,30 +1,9 @@
-let removePopup = `<div class="sonata-ba-delete popup-file-delete mfp-hide">
-                        <div class="box box-danger">
-                            <div class="box-header">
-                                <h3 class="box-title">Bevestig verwijdering</h3>
-                            </div>
-                            <div class="box-body">
-                                Weet je zeker dat de geselecteerde afbeelding moet worden verwijderd?
-                            </div>
-                            <div class="box-footer clearfix">
-                                <button class="btn btn-danger js-trigger-save"><i class="fa fa-trash" aria-hidden="true"></i> Ja, verwijder</button>
-                                <button class="btn btn-success js-trigger-cancel" href="/admin/wearejust/example/post/1/edit">
-                                <i class="fa fa-pencil" aria-hidden="true"></i>
-                                Nee, annuleer</button>
-                            </div>
-                        </div>
-                        <button title="Close (Esc)" type="button" class="mfp-close js-trigger-cancel">×</button>
-                        <div class="anim-loading">
-                            <label></label>
-                            <label></label>
-                            <label></label>
-                            <label></label>
-                        </div>
-                    </div>`;
-
 class imageRemove {
     constructor(item) {
-        $('body').append(removePopup);
+        $.get('/wearejust/sonata_theme/delete_dialog', function(html) {
+            $('body').append(html);
+        });
+        
         this.item = item
         this.item.find('input[type="checkbox"]').on('change', this.change.bind(this));
         $('.js-trigger-save').on('click', forceSave);
