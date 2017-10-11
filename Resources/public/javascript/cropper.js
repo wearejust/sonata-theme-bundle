9fec5fe41474200472dbee0b0f9aa7cc0b3f7cf6
Bart Koelewijn <bart.koelewijn@wearejust.com>
2017-10-11 14:14:30 +0200

    Chore: Some extra refactor points for the cropper

--- Resources/public/javascript/cropper.js
+++ Resources/public/javascript/cropper.js
@@ -1,24 +1,26 @@
-class Cropper {
-    constructor(item) {
-        this.item = item;
-        this.item.find($('*[data-method="getCroppedCanvas"]')).on('click', this.save.bind(this));
-    } 
-
-    save() {
-        let updateBtn = $('*[name="btn_update_and_edit"]');
-        if(!updateBtn.length) return;
-        
-    	setTimeout(function() {
-    		updateBtn.click();
-    	}, 500); 
-    }
-}
+let updateExitBtn;
 
 $(function(){
+    updateExitBtn = $('*[name="btn_update_and_edit"]');
+    if(!updateExitBtn.length) return;
+
     $('.cropper').each(function(index,item){
         new Cropper($(item));
     });
 });
+
+class Cropper {
+    constructor(item) {
+        this.item = item;
+        this.item.find('*[data-method="getCroppedCanvas"]').on('click', this.save.bind(this));
+    } 
+
+    save() {
+    	setTimeout(function() {
+    		updateExitBtn.click();
+    	}, 500); 
+    }
+}
 
 
 
