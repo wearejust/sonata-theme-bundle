87d6f87fe0530c8e1530f8a9105c19996b69d55e
Rutger Kooijman <rutger.kooijman@wearejust.com>
2018-02-01 16:22:19 +0100

    Feature: ajax submit list, show submit on checked box

--- Resources/assets/javascript/popup.js
+++ Resources/assets/javascript/popup.js
@@ -4,4 +4,29 @@ $(function(){
 	    closeBtnInside: true,
 	    mainClass: 'mfp-fade'
 	});
+
+	$('.js-list-table form').submit(function(event){
+        event.preventDefault();
+        var $form = $(this);
+
+        $.ajax({
+            type: 'POST',
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            success: function(data) {
+                var $elm = $('#formResult');
+                $elm.html(data);
+                $.magnificPopup.open({
+                    items: {
+                        src: $elm.html(),
+                        type: 'inline'
+                    }
+                });
+
+            },
+            error: function(error) {
+                // Do something with the error
+            }
+        });
+    });
 });
